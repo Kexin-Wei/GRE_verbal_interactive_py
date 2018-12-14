8757e3057a573e8579895922477b98ab608d9c2e
Vacationwkx <wkx611@outlook.com>
2018-12-14 14:49:58 +0800

    welcome to try and feedback

--- READE.md
+++ READE.md
@@ -1,1 +1,30 @@
 # GRE_verbal_interactive_py
+
+A small program showing in terminal, using interactive mode to increase the concentration
+
+## Some need to know
+
+- based on python3
+
+- package needed : xlrd, xlwt,xltuils,(operator)
+
+  *for linux*
+
+  ```
+  pip3 install xlrd
+  pip3 install xlwt
+  pip3 install xltuils
+  (optional)
+  pip3 install operator
+  ```
+## How to use
+
+ 0 . choose continuing learning or starting a bra new turn
+
+ 1 . choose the file by typing the number in terminal
+
+ 2 . choose the study mode
+    - fast view mode
+    - typing correct mode
+
+ 3 . type "stop!" to exit and the record will be noted
--- gre_read.py
+++ gre_read.py
@@ -1,104 +1,138 @@
 import xlrd
-import random
 import operator
 import xlwt
 from xlutils.copy import copy
 import os
+import platform
 
-xlsfile=r"verbal/6.xls"
-xlsfile1=r"verbal/xdf.xls"
-xlsfile2=r"verbal/再要你命3000核心词汇考法精析.xls"
-xlsfile3=r"verbal/GRE短语乱序.xlsx"
+
+xlsfile=r"6.xls"
+xlsfile1=r"xdf.xls"
+xlsfile2=r"再要你命3000核心词汇考法精析.xls"
+xlsfile3=r"GRE短语乱序.xlsx"
 file=[xlsfile,xlsfile1,xlsfile2,xlsfile3]
 
-index=int(input("Choose which one to learn:\n1. 6 stufe\n2. xdf 6 stufe\n3. 3000\n4. phrase\n"))-1
+# choose file
+index=int(input(" Choose which one to learn:\n \
+1. 6 stufe\n \
+2. xdf 6 stufe\n \
+3. 3000\n \
+4. phrase\n"))-1
+
+#file pyth operation
 pypath=os.path.dirname(__file__)
 if pypath :
-    path=os.path.dirname(__file__)+'/'+file[index]
+    if platform.system()=="Linux":
+        path=os.path.dirname(__file__)+'/'+file[index]
+    if platform.system()=="Windows":
+        path=os.path.dirname(__file__)+'\\'+file[index]
 else:
     path=file[index]
 
+# get data from file
 book=xlrd.open_workbook(path,"rb")
 sheet0=book.sheet_by_index(0)
 sheet1=book.sheet_by_index(1)
 nrow=sheet0.nrows
-#print(nrow)
 
-bookcp=copy(book)
-sheet1w=bookcp.get_sheet(1)
-
-
-
-word=sheet0.col_values(0)
+# get explanation from different file
 if index !=3:
     explanation=sheet0.col_values(1)
 #print(explanation)
 if index==1 or index==0:
-    explanation2=sheet0.col_values(2)  #2,3 for xdf.xls
+    explanation2=sheet0.col_values(2)  #2,3 for 6.xls and xdf.xls
     explanation3=sheet0.col_values(3)
 
+#set write excel
+bookcp=copy(book)
+sheet1w=bookcp.get_sheet(1)
+
+# read last time record
+word=sheet0.col_values(0)
+num=int(sheet1.cell(0,0).value) # last time stop at round num
+
+#set the list
 num_list=list(range(nrow))
-#random.shuffle(num_list) #uncomment except 6.xlsx
 
-num=int(sheet1.cell(0,0).value)
+#choose continue or start a new turn
+start=input("\n###########################\n \
+Would you want continue?\n \
+(type 'no' for a new turn)\n\
+###########################\n")
 
+if operator.eq(start,"no") :
+    sheet1w.write(0,0,'0')
+    num=0
+    print(" Start a new turn now!!")
+else:
+    print("\n^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^\n \
+Continue at : Round ",num+1,"\n\
+^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^\n")
 
-if num!=0 :
-    print("\n^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^\n  Continue at : Round ",num+1,"\n^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^\n")
-    if num in num_list:
-        a=num
-        #print(a)
+#choose study mode
+# 1 typing and recorrecting mode
+# 2 fast view mode
+mode=int(input("\n****************************\n \
+Choose the study mode :\n \
+1. Typing correct mode\n \
+2. Fast view mode\n\
+****************************\n"))
+if mode==1 and index==3:
+    print(" Sorry for GRE短语乱序.slx there is only a fast view mode\n")
+    input(" Type any key to continue :")
+    mode=2
 
 for x in num_list:
+    again=0
     if num and x<num :
         pass
     else:
-        #************** 6 stufe*************************
         print("%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%")
         word[x]=word[x].rstrip()
         print(" Round ",x+1,'/',nrow,'\n')
-        if index==0:
-            #print modify mode word,exp->word
+
+        #6 stufe and xdf 6 stufe
+        if index==0 or index==1:
             print(' ',word[x],"\n ===\n",explanation[x],'\n',explanation2[x],'\n',explanation3[x])
+        #3000
+        if index==2:
+            print(' ',word[x],'\n ===')
+            for y in explanation[x].split('；'):
+                print(y)
+        # phrase
+        if index==3:
+            print(' ',word[x])
+
+        # mode 1 : reprint mode
+        if mode==1:
             str=input("\n Please reprint :")
 
-            # uncomment for fast mode
-            if operator.eq(str,"stop!") :
-                break
-            # end uncomment
-
             while operator.ne(str,word[x]) :
+                if operator.eq(str,"stop!") :
+                    break
+                again=1
                 print("\n!! Wrong !!\n")
                 print(' ',word[x],'\n')
                 str=input(" Please reprint :")
-                if operator.eq(str,"stop!") :
-                    break
 
-            """#comment for fast mode
-            flag=input("Continue ? (y/n)")
-            if operator.eq(flag,"n") :
+            if again:
+                str=input("\n Again to testify :")
+                while operator.ne(str,word[x]) :
+                    if operator.eq(str,"stop!") :
+                        break
+                    print("\n!! Wrong !!\n")
+                    print(' ',word[x],'\n')
+                    str=input(" Please reprint :")
+
+
+            if operator.eq(str,"stop!") :
                 break
-            """
 
-        else:#menmorized mode
-            #*************** xdf ************************
-            if index==1:
-                print(' ',word[x],"\n ===\n",explanation[x],'\n',explanation2[x],'\n',explanation3[x])
-
-            #***************** 3000 *************************
-            if index==2:
-                print(' ',word[x],'\n ===')
-                for y in explanation[x].split('；'):
-                    print(y)
-
-            #************** phrase ****************
-            if index==3:
-                print(' ',word[x])
-
+        # mode 2: fasr view mode
+        if mode==2:
             flag=input("\n Waiting.....")
             if operator.eq(flag,"stop!") :
                 break
-
     a=x
 
 if a+1==nrow:

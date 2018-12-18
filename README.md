5c601c598f0cb8ec3c4aa5b35fda9319060b6e48
Vacationwkx <wkx611@outlook.com>
2018-12-18 14:58:01 +0800

    add find function

--- README.md
+++ README.md
@@ -17,23 +17,36 @@ A small program showing in terminal, using interactive mode to increase the conc
 ## How to use
 
 1. Choose continuing learning or starting a bra new turn.
-   - type 'no' for a new one
-   - press any key to continue
-
+   ```
+   1. type 'no' for a new one
+   2. press any key to continue
+   ```
 2. Choose the file by typing the number in terminal:
+   ```
    1. 6 stufe
    2. xdf 6 stufe
    3. 3000
    4. Phrase
-
+   ```
 3. Choose the study mode:
+  ```
    - typing correct mode(default)
    - fast view mode
+  ```
+4. Type ":s" at any place to exit and the record will be noted.
 
-4. Type "stop!" at any place to exit and the record will be noted.
+5. Type ":f XXX" out of review mode, to find the "XXX" in any word, and show the relative explanation, like :
+    ```
+    Please reprint: :f a
+    >
+    annoy
+    banality
+    ...
+    ```
+6. After every 5 words will be stopped and maybe review for better equality, also file autosaved.
 
-5. After every 5 words will be stopped and maybe review for better equality, also file autosaved.
-   - in the review, when word is showed, remind the meaning of it and type any key to continue reviewing
+### Tips :
+   > in the review, when word is showed, remind the meaning of it and type any key to continue reviewing
 
 ## Special alphabet
   - é : use e/
--- gre_read.py
+++ gre_read.py
@@ -8,36 +8,70 @@ import platform
 import re
 #********************************************************************************
 #**************** replace spanish *****************
-def comp( input, word, wordlist):
-    if word.find('\u00E9')>=0:
+def inputjudge( outprint, x, index, wordlist,explanation):
+    # input sepcial word progress
+    in=input(outprint)
+    if wordlist[x].find('\u00E9')>=0:
         #print(operator.eq(input,word),1,input.replace("e/",'\u00E9'))
-        return operator.eq(input.replace("e/",'\u00E9'),word)
-    if word.find('\u00E8')>=0:
+        operator.eq(input.replace("e/",'\u00E9'),word)
+    if wordlist[x].find('\u00E8')>=0:
         #print(operator.eq(input,word),2,input.replace("e\\",'\u00E8'))
-        return operator.eq(input.replace("e\\",'\u00E8'),word)
-    if word.find('\u00EF')>=0:
-        return operator.eq(input.replace("i..",'\u00EF'),word)
-    if input.find(":find ")>=0 and wordlist!=0:#close function
-        search=input.split(' ',1)[1]
-        #print("here")
-        x=findw(search,wordlist)
-    #print(operator.eq(input,word),3)
-    return operator.eq(input,word)
+        operator.eq(input.replace("e\\",'\u00E8'),word)
+    if wordlist[x].find('\u00EF')>=0:
+        operator.eq(input.replace("i..",'\u00EF'),word)
 
-#******************find the word *****************
-def findw(sword,words):
-    for aim in words:
+    if in.find(":f ")>=0:#close function
+        sword=in.split(' ',1)[1]
+        #print("here")
+        findw(sword,index,wordlist,explanation)
+        #need to input judge agian
+        return inputjudge("\n Please reinput again: ",x,index,wordlist,explanation)
+    elif input.find(":s ")>=0:
+        return ":s"
+    else:
+    #print(operator.eq(input,word),3)
+        return operator.eq(input,wordlist[x])
+
+#****************** find the word *****************
+def findw(sword,index,wordlist,explanation):
+    for aim in wordlist:
         aim2=aim.rstrip()
+        # special word progress
         if sword.find('*')>=0:
             search=r""+sword.replace('*',"\*")
         else:
             search=r""+sword
         if re.search(search,aim2):
-            x=words.index(aim)
-            print(" "+aim)
+            x=wordlist.index(aim)
+            printwe(x,index,wordlist,explanation)
             flagg=1
     if flagg==0:
         print("No such a word")
+    return
+
+#******************* print ************************
+def printwe(x,index,wordlist,explanation):
+    #nexplana=len(explana)
+    #****6 stufe and xdf 6 stufe******
+    if index==0 or index==1:
+        print(' ',wordlist[x])
+        print(explanation[1][x],'\n',explanation[2][x],'\n',explanation[3][x])
+    #************* 3000*************
+    if index==2:
+        input(' \u3010'+wordlist[x]+'\u3011')
+        z=1
+        if explanation[1][x].find('\u003b')>0:
+            #print("Get this")
+            for y in explanation[1][x].split(';'):
+                print(' ',z,'.'+y.lstrip())
+                z=z+1
+        else:
+            for y in explanation[1][x].split('\uff1b'):
+                print(' ',z,'.'+y)
+                z=z+1
+    #*********** phrase**************
+    if index==3:
+        print(' ',wordlist[x])
     return
 #**********************************************************************************
 #**********************************************************************************
@@ -49,14 +83,14 @@ xlsfile3=r"GRE短语乱序.xlsx"
 file=[xlsfile,xlsfile1,xlsfile2,xlsfile3]
 
 
-# choose file
+#****************** choose file********************
 index=int(input(" Choose which one to learn:\n \
 1. 6 stufe\n \
 2. xdf 6 stufe\n \
 3. 3000\n \
 4. phrase\n"))-1
 
-#file pyth operation
+#************* file pyth operation********************
 pypath=os.path.dirname(__file__)
 if pypath :
     if platform.system()=="Linux":
@@ -66,32 +100,37 @@ if pypath :
 else:
     path=file[index]
 
-# get data from file
+#******************** get data from file******************
 book=xlrd.open_workbook(path,"rb")
 sheet0=book.sheet_by_index(0)
 sheet1=book.sheet_by_index(1)
 nrow=sheet0.nrows
 
-# get explanation from different file
+#****************** get explanation from different file *****
+explanation=[[]]
 if index !=3:
-    explanation1=sheet0.col_values(1)
+    #explanation1=sheet0.col_values(1) #len=2[0,1]
+    explanation.append(sheet0.col_values(1))
 #print(explanation)
 if index==1 or index==0:
-    explanation2=sheet0.col_values(2)  #2,3 for 6.xls and xdf.xls
-    explanation3=sheet0.col_values(3)
+    #explanation2=sheet0.col_values(2)  #2,3 for 6.xls and xdf.xls
+    #explanation3=sheet0.col_values(3)  #len=4[0,1,2,3]
+    explanation.append(sheet0.col_values(2))
+    explanation.append(sheet0.col_values(3))
 
-#set write excel
+#************* set write excel********************
 bookcp=copy(book)
 sheet1w=bookcp.get_sheet(1)
 
-# read last time record
-word=sheet0.col_values(0)
+#************ read last time record********************
+wordlist=sheet0.col_values(0)
 num=int(sheet1.cell(0,0).value) # last time stop at round num
 
-#set the list
+#*********** set the list************************
 num_list=list(range(nrow))
 
-#********************* choose study mode******************
+#********************************************************************************
+#******************* choose study mode*******************
 # 1 typing and recorrecting mode(default)
 # 2 fast view mode
 temp=input("\n************************************\n \
@@ -107,6 +146,7 @@ if mode==1 and index==3:
     input(" Type any key to continue :")
     mode=2
 
+#******************************************************************************
 #************* choose continue or start a new turn*****************
 start=input("\n###################################\n \
 Would you want continue?\n \
@@ -125,80 +165,58 @@ Continue at : Round ",num+1,"\n\
 #for pause after 5 words
 pause=0
 review=""
-#start learning
+#***************** start learning ****************
 for x in num_list:
     again=0
+    a=x-1
     if num and x<num :
         pass
     else:
 #********* rest********************
-        # rest
         if pause > 4:
             sheet1w.write(0,0,a)
             bookcp.save(path)
             print("=======\n Maybe a rest and review?")
             for y in review.split('|'):
-                stop=input("  "+y)
-                if comp(stop,"stop!",0):
+                stop=inputjudge("  "+y,x,index,wordlist,explanation)
+                if operator.eq(stop,":s"):
                     break
-            if comp(stop,"stop!",0):
+            if operator.eq(stop,":s"):
                 break
             review=""
             pause=0
-        word[x]=word[x].rstrip()
+        wordlist[x]=wordlist[x].rstrip()
         if pause==4 :
-            review=review+word[x]
+            review=review+wordlist[x]
         else:
-            review= review+word[x]+'|'
+            review= review+wordlist[x]+'|'
         print("\n Round ",x+1,'/',nrow)
         print("&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&")
 #***************** file ****************************
-        #****6 stufe and xdf 6 stufe******
-        if index==0 or index==1:
-            print(' ',word[x],"\n",explanation1[x],'\n',explanation2[x],'\n',explanation3[x])
-        #************* 3000*************
-        if index==2:
-            input(' \u3010'+word[x]+'\u3011')
-            z=1
-            if explanation1[x].find('\u003b')>0:
-                #print("Get this")
-                for y in explanation1[x].split(';'):
-                    print(' ',z,'.'+y.lstrip())
-                    z=z+1
-            else:
-                for y in explanation1[x].split('\uff1b'):
-                    print(' ',z,'.'+y)
-                    z=z+1
-        #*********** phrase**************
-        if index==3:
-            print(' ',word[x])
+        printwe(x,index,wordlist,explanation)
 #***************** mode *********************************
         # mode 1 : reprint mode
         if mode==1:
-            str=input(" Please reprint :")
-
-            #print(comp(str,word[x]),0)
-
-            while comp(str,word[x],word)==0:
-                while comp(str,word[x],0)==0:
-                    if comp(str,"stop!",0) :
-                        break
+            ToF=inputjudge(" Please reprint :", x, index, wordlist,explanation)
+            if operator.eq(ToF,":s") :
+                break
+            while ToF:
+                while ToF:
                     print(" !!! Wrong !!!")
-                    print(' ',word[x])
-                    str=input(" Please reprint :")
-                if comp(str,"stop!",0) :
+                    print(' ',wordlist[x])
+                    ToF=inputjudge(" Please reprint :", x, index, wordlist,explanation)
+                if operator.eq(ToF,":s") :
                     break
-                str=input(" Again to testify :")
-            if comp(str,"stop!",0) :
+                ToF=inputjudge(" Again to testify :", x, index, wordlist,explanation)
+            if operator.eq(ToF,":s") :
                 break
 
         # mode 2: fasr view mode
         if mode==2:
-            flag=input("\n Waiting.....")
-            if comp(flag,"stop!",word) :
+            flag=inputjudge(" Waiting.....", x, index, wordlist,explanation)
+            if operator.eq(flag,":s") :
                 break
         pause=pause+1
-    a=x
 
 
 if a+1==nrow:
